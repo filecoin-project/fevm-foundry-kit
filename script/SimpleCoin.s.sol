d28951515ee2dbeff4a42474a0c00d0b5dc7fe94
Nick Lionis <nick@fil.builders>
2025-08-04 15:29:35 +0300

    feat: refactor kit to use foundry best practices and improve tooling (#40)
    * feat: foundry kit enhancements
    
    * fix: add package locks in gitignore
    
    * docs: updated README.md

--- script/SimpleCoin.s.sol
+++ script/SimpleCoin.s.sol
@@ -2,27 +2,21 @@
 pragma solidity ^0.8.11;
 
 import "@forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+
 import "../src/basic-solidity-examples/SimpleCoin.sol";
 
 contract MyScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address yourAddress = vm.addr(deployerPrivateKey);
+        console.log("Deployer address: ", yourAddress);
         vm.startBroadcast(deployerPrivateKey);
 
         SimpleCoin coin = new SimpleCoin();
 
         // Do whatever you want!
 
-        uint256 balance = coin.getBalance(yourAddress);
-        console.log("Your balance is: ", balance);
-
-        bool result = coin.sendCoin(yourAddress, 100);
-        console.log("Coin successfully sent: ", result);
-
-        balance = coin.getBalance(yourAddress);
-        console.log("Your new balance is: ", balance);
-        
         vm.stopBroadcast();
     }
 }
